38c230a4b7d6edea2017c20d91ee4fcfb18b7e88
svahir <shubham.ahir@oracle.com>
2024-05-23 17:24:00 +0530

    Response Entity Info added

--- src/main/java/com/shubham/springboot/journal/controllers/JournalEntryController.java
+++ src/main/java/com/shubham/springboot/journal/controllers/JournalEntryController.java
@@ -1,6 +1,8 @@
 package com.shubham.springboot.journal.controllers;
 
 import com.shubham.springboot.journal.models.JournalEntry;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,20 +12,36 @@ import java.util.Map;
 
 @RestController//@Component and @ResponseBody -serialize object to http response
 @RequestMapping("journal")
+
 public class JournalEntryController {
 
     private Map<Long, JournalEntry> journeyEntries = new HashMap<>();
 
-    @GetMapping("all")
-    public List<JournalEntry> getAll(){
+    //@ResponseEntity helps us to customize http response using header, body and status code
 
-        return new ArrayList<>(journeyEntries.values());
+    @GetMapping("all")
+    public ResponseEntity<List<JournalEntry>> getAll(){
+
+        List<JournalEntry> journalEntries= new ArrayList<>(journeyEntries.values());
+        return new ResponseEntity<List<JournalEntry>>(journalEntries, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<JournalEntry> getById(@PathVariable Long id){
+
+        JournalEntry journalEntry= journeyEntries.get(id);
+        if(journalEntry!=null){
+            return new ResponseEntity<JournalEntry>(journalEntry, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
     @PostMapping("add")
-    public Boolean createEntry(@RequestBody JournalEntry entry){
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry entry){
         journeyEntries.put(entry.getId(), entry);
-        return true;
+        return new ResponseEntity<>(entry,HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
